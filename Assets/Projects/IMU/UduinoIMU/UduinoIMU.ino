d8f718a622fd7f1d76611205887f2c62153f4503
Guilherme Brendel <guilhermebrendel@gmail.com>
2018-04-25 14:46:46 -0300

    Included Uduino, added firt gyroscope functionality.

---
+++ Assets/Projects/IMU/UduinoIMU/UduinoIMU.ino
@@ -0,0 +1,229 @@
+#include "Uduino.h"  // Include Uduino library at the top of the sketch
+Uduino uduino("IMU");
+
+#define OUTPUT_READABLE_QUATERNION
+// #define OUTPUT_READABLE_WORLDACCEL
+//#define OUTPUT_READABLE_EULER
+//#define OUTPUT_READABLE_YAWPITCHROLL
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+
+int prevVal = 0;
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  pinMode(12, INPUT_PULLUP);
+
+  Serial.begin(38400);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  devStatus = mpu.dmpInitialize();
+
+  mpu.setXGyroOffset(54); //++
+    mpu.setYGyroOffset(-21); //--
+    mpu.setZGyroOffset(5);
+
+  if (devStatus == 0) {
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // Error
+    Serial.println("Error!");
+  }
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+  uduino.update();
+
+  if (!dmpReady) {
+    if (uduino.isInit()) {
+      Serial.println("IMU not ready !");
+    }
+    return;
+  }
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+      
+    }
+
+  if (uduino.isInit()) {
+
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+      // reset so we can continue cleanly
+      mpu.resetFIFO();
+      // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+      // wait for correct available data length, should be a VERY short wait
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      // read a packet from FIFO
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+
+      int v = digitalRead(12);
+      if (v != prevVal) {
+
+        if (v == 0) Serial.println("buttonUp");
+        else if (v == 1) Serial.println("buttonDown");
+
+        prevVal = v;
+      }
+
+#ifdef OUTPUT_READABLE_QUATERNION
+      // display quaternion values in easy matrix form: w x y z
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      Serial.print("r/");
+      Serial.print(q.w);
+      Serial.print("/");
+      Serial.print(q.x);
+      Serial.print("/");
+      Serial.print(q.y);
+      Serial.print("/");
+      Serial.print(q.z);    
+      Serial.println();
+#endif
+
+#ifdef OUTPUT_READABLE_EULER
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetEuler(euler, &q);
+      Serial.print(euler[0] * 180 / M_PI);
+      Serial.print("/");
+      Serial.print(euler[1] * 180 / M_PI);
+      Serial.print("/");
+      Serial.println(euler[2] * 180 / M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      Serial.print(ypr[0] * 180 / M_PI);
+      Serial.print("/");
+      Serial.print(ypr[1] * 180 / M_PI);
+      Serial.print("/");
+      Serial.println(ypr[2] * 180 / M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_REALACCEL
+      // display real acceleration, adjusted to remove gravity
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      Serial.print("a/");
+      Serial.print(aaReal.x);
+      Serial.print("/");
+      Serial.print(aaReal.y);
+      Serial.print("/");
+      Serial.println(aaReal.z);
+#endif
+
+#ifdef OUTPUT_READABLE_WORLDACCEL
+      // display initial world-frame acceleration, adjusted to remove gravity
+      // and rotated based on known orientation from quaternion
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+      Serial.print("a/");
+      Serial.print(aaWorld.x);
+      Serial.print("/");
+      Serial.print(aaWorld.y);
+      Serial.print("/");
+      Serial.println(aaWorld.z);
+#endif
+      // blink LED to indicate activity
+      blinkState = !blinkState;
+      digitalWrite(LED_PIN, blinkState);
+    }
+  }
+}
---
+++ Assets/Uduino/Examples/Advanced/MultipleArduino/MultipleArduino.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using Uduino;
+
+public class MultipleArduino : MonoBehaviour
+{
+    UduinoManager u;
+    int sensorOne = 0;
+    int sensorTwo = 0;
+
+    void Start()
+    {
+        //Set up the pinMode of the boards should not be done on Start() or Awake() 
+        // but on the delegate function OnBoardConnected(). It should be called only if
+        // you use several boards with the default Uduino arduino sketch.
+        UduinoManager.Instance.OnBoardConnected += OnBoardConnected;
+        UduinoManager.Instance.OnValueReceived += OnValuesReceived;
+    }
+
+    void Update()
+    {
+        if (UduinoManager.Instance.hasBoardConnected())
+        {
+            UduinoDevice firstDevice = UduinoManager.Instance.GetBoard("myArduinoName");
+            UduinoDevice secondDevice = UduinoManager.Instance.GetBoard("myOtherArduino");
+            UduinoManager.Instance.Read(firstDevice, "myVariable");
+            UduinoManager.Instance.Read(secondDevice, "mySensor");
+            Debug.Log(sensorOne);
+            Debug.Log(sensorTwo);
+        }
+    }
+
+    void OnValuesReceived(string data, UduinoDevice device)
+    {
+        if (device.name == "myArduinoName") sensorOne = int.Parse(data);
+        else if (device.name == "myOtherArduino") sensorTwo = int.Parse(data);
+    }
+
+    void OnBoardConnected(UduinoDevice connectedDevice)
+    {
+        if (connectedDevice.name == "uduinoBoard")
+        {
+            // Set the pinMode of the first board here.
+            //  UduinoManager.Instance.pinMode(connectedDevice, 13, PinMode.Input_pullup);
+            //
+            // You can also add extra settings : 
+            // connectedDevice.alwaysRead = false;
+            // connectedDevice.readTimeout = 50;
+        }
+        else if (connectedDevice.name == "uduinoBoard2")
+        {
+            // Set the pinMode of the second board here.
+            //  UduinoManager.Instance.pinMode(connectedDevice, 12, PinMode.Output);
+        }
+    }
+}
